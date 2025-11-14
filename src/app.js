8a8599f19dfe7b653e60ae890f5f3d439e1d1a61
Manav Mittal <manavmittal451@gmail.com>
2025-11-14 15:56:59 +0530

    Learning data sanitization and exploring validator library

--- src/app.js
+++ src/app.js
@@ -1,45 +1,103 @@
-//my chnages
-
 const express = require("express");
 const app = express();
 const {connectDB} = require("./config/database");
-const User = require("./models/user")
+const User = require("./models/user");
 
 
-// app.post("/signUp", async (req,res) => {
-// const user = new User({
-//     firstName: "Manav",
-//     lastName: "Mittal",
-//     email: "manav@gmail.com",
-//     password: "manav@123"
-// });
-// await user.save();
-// });
+app.use(express.json());
 
-
+// Adding user in database
 app.post("/signUp", async (req,res) => {
-    const user = new User ({
-        firstName: "Rahul",
-        lastName: "Kumar",
-        emailId: "rahul@gmail.com",
-        password: "rahul@123"
-    });
-
-    
+    const user = new User(req.body);
     try{
         await user.save();
         res.send("User added successfully!");
-    }catch(err){
-        res.status(400).send("Error saving the user:" + err.message)
+    }
+    catch(err){
+        console.error("User cannot added!");
+        res.status(400).send("User cannot added: " + err.message);
+    }
+});
+
+// Get user by emailId
+app.get("/user", async (req,res) => {
+    const userEmail = req.body.emailId;
+    try{
+        const users = await User.findOne({emailId: userEmail});
+        if(userEmail.length === 0){
+            res.status(404).send("User not found!");
+        }
+        else{
+            res.send(users);
+        }
+    }
+    catch(err){
+        res.status(500).send("Something went wrong!");
+    }
+})
+
+// Getting all users
+app.get("/feed", async (req,res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(500).send("Something went wrong!");
+    }
+})
+
+// Delete users by there userId
+app.delete("/user/:userId", async (req,res) => {
+    const userId = req.params.userId;
+    try{
+        await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully!");
+    }
+    catch(err){
+        res.status(400).send("Something went wrong!");
     }
 });
 
 
-connectDB().then(() => {
-    console.log("Database connection established...");
-    app.listen(7777, () => {
-        console.log("Server is listning on port 7777...");
-    })
-}).catch((err) => {
-    console.log("Database cannot connected!");
+// Update user by there userId
+app.patch("/user/:userId", async (req,res) => {
+    const userId = req.params.userId;
+    const data = req.body;
+    try{
+
+        const UPDATE_ALLOWED = ["firstName", "lastName", "age", "gender", "country", "about", "skills"];
+        const isUpdateAllowed = Object.keys(data).every((k) => 
+            UPDATE_ALLOWED.includes(k)
+        );
+
+        if(!isUpdateAllowed){
+            throw new Error("Update not allowed!");
+        }
+
+        if(data?.skills.length > 10){
+            throw new Error("Skills cannot be more than 10!");
+        }
+
+
+        await User.findByIdAndUpdate(userId, data, {returnDocument: "after", runValidators: true});
+        res.send("Data updated successfully!");
+    }
+    catch(err){
+        res.status(400).send("UPDATE FAILED: " + err.message);
+    }
 })
+
+connectDB().then(() => {
+    try{
+        console.log("Database connected succsefully...");
+        app.listen(3000, () => {
+            console.log("Server listening on port 3000...");
+        })
+    }
+    catch(err){
+        console.error("Database cannot connected!");
+    }
+})
+
+    
--- src/models/user.js
+++ src/models/user.js
@@ -1,21 +1,78 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 50,
+        validate(value){
+            const regex = /^[A-Za-z\s\-']+$/;
+            if(!regex.test(value)){
+                throw new Error("First name can only contain letters, spaces, hyphens, and apostrophes");
+            }
+        }
     },
     lastName: {
-        type: String
+        type: String,
+        validate(value){
+            const regex = /^[A-Za-z\s\-']+$/;
+            if(!regex.test(value)){
+                throw new Error("Last name can only contain letters, spaces, hyphens, and apostrophes");
+            }
+        }
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Please enter the valid email address!");
+            }
+        }
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        validate(value) {
+        if(!validator.isStrongPassword(value)){
+            throw new Error("Please enter the strong password! " + value);
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18,
     },
     gender: {
-        type: String
-    }
-});
+        type: String,
+        validate(value){
+            if(!["male", "female", "others"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        },
+    },
+    photoUrl: {
+        type: String,
+    },
+    about: {
+        type: String,
+        default: "This is the default about of user!",
+    },
+    skills: {
+        type: [String],
+    },
+},
+{
+    timestamps: true,
+}
+);
+
+userSchema.index({ email: 1 }, { unique: true });
 
 const userModel = mongoose.model("User", userSchema);
 module.exports = userModel;
