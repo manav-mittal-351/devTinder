272cee7d4050ab926b4176748043387f8084b85a
Manav Mittal <manavmittal451@gmail.com>
2026-02-23 19:15:38 +0530

    Add indexing in database

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -21,7 +21,7 @@ const connectionRequestSchema = new mongoose.Schema({
     timestamps: true,
 });
 
-
+connectionRequestSchema.index({senderUserId: 1, receiverUserId: 1});
 
 connectionRequestSchema.pre("save", function(next){
     const connectionRequest = this;
